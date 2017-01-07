42f77542679095ebbe18090a0e566bdd3da2bbcd
juju4 <juju4@users.noreply.github.com>
2017-01-07 04:42:33 -0500

    sync repositories

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -5,6 +5,13 @@ umask 022
 
 rolesdir=$(dirname $0)/..
 
-[ ! -d $rolesdir/lxd ] && git clone https://github.com/juju4/ansible-lxd $rolesdir/lxd
-[ ! -d $rolesdir/redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/redhat-epel
+[ ! -d $rolesdir/juju4.lxd ] && git clone https://github.com/juju4/ansible-lxd $rolesdir/juju4.lxd
+[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.lxcpublish ] && ln -s ansible-lxcpublish $rolesdir/juju4.lxcpublish
+[ ! -e $rolesdir/juju4.lxcpublish ] && cp -R $rolesdir/ansible-lxcpublish $rolesdir/juju4.lxcpublish
+
+## don't stop build on this script return code
+true
+
 
